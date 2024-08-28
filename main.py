eb27b7260527f47dfdb6854b17527be0b4b8cb66
Sudo-Self <sudo-self@proton.me>
2024-08-28 11:48:55 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from usbswitcher import samsungGalaxyToModemMode
 from at_utils import enableADB
 from adb_utils import waitForDevice, uploadAndRunFRPBypass
-v def main():
-samsungGalaxyToModemMode ( )
-enableADB( )
-waitForDevice ( )
-uploadAndRunFRPBypass ()
-if
-__name__ == "__main__":
-main ( )
+
+def main():
+    samsungGalaxyToModemMode()
+    enableADB()
+    waitForDevice()
+    uploadAndRunFRPBypass()
+
+if __name__ == "__main__":
+    main()
+
